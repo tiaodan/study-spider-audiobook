f50022024230e1c629a3a01ed5a1149fae10cdd3
tiaodan <tiaodanbang@gmail.com>
2025-04-11 07:36:34 +0000

    v0.0.0.1 整合错误统一处理, 在errorutil文件夹

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"log"
 	"study-spider-audiobook/config"
 	"study-spider-audiobook/logger"
-
-	"github.com/spf13/viper"
 )
 
-// 初始化
+// 初始化, 默认main会自动调用本方法
 func init() {
 	// 获取配置实例（首次调用时触发初始化）
 	cfg := config.GetConfig(".", "config", "yaml")
@@ -29,12 +26,18 @@ func init() {
 
 	// 打印配置
 	logger.Debug("network.ximalayaIIp_ip: %s", cfg.Network.XimalayaIIp)
-	viper.Set("network.ximalaya_Ip", "www.baidu.com")
-	if err := config.WriteConfig2Blank(".", "config", "yaml"); err != nil {
-		// if err := config.WriteConfig4Blank(cfg); err != nil {
-		log.Fatalln("写入配置文件失败,err: ", err)
-	}
 }
-func main() {
 
+/*
+思路:
+ 1. 读取配置文件， (如果配置文件不填, 自动会有默认值)
+ 2. 设置日志级别, 默认info
+ 3. 统一调用错误打印, 封装函数
+ 4. 爬取页面数据, 尽量去重
+ 5. 插入数据库
+*/
+func main() {
+	// 1. 读取配置文件， (如果配置文件不填, 自动会有默认值)
+	// 2. 设置日志级别, 默认info
+	// 3. 统一调用错误打印, 封装函数
 }
---
+++ ximalaya/ximalay.go
@@ -0,0 +1,14 @@
+package ximalaya
+
+// 爬取喜马拉雅【广播剧】, 类似接口的方式。
+/*
+思路:
+1. 人工用浏览器F12找到某一个类别的请求（如：文学名著）
+2. 把该链接url, 作为方法参数
+
+步骤:
+1. 请求url, 获取json数据
+*/
+func spiderGuangbojuByInterface(url string) {
+
+}
